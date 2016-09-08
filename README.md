380974d86b417f8f72bb1ef0a9aa23768ca5c48a
Tanveer Hassan <tanveer.hassan@sekai-lab-bd.com>
2016-09-08 17:09:54 +0600

    creating a controller will now add it to the index.js file

--- README.md
+++ README.md
@@ -4,11 +4,17 @@
 npm install -g yo
 sudo npm link
 </pre>
-#create module
+## create module
 <pre>
 yo ng-yeo
 </pre>
-#create module controller
+## create module controller
 <pre>
 yo ng-yeo:controller
 </pre>
+The prompt will ask for `moduleName.ctrlName`
+
+e.g., There is a module named `foo` and folder named `foo-module`,
+and the controller is to be named `helloWorld`.
+
+The correct prompt answer should then be `foo.helloWorld`; this will create a controller file `hello-world-controller.js`.
--- controller/index.js
+++ controller/index.js
@@ -1,30 +1,43 @@
-var generators = require('yeoman-generator');
-var options = {};
+const generators = require('yeoman-generator');
+const _ = require('lodash');
 
+let options = {};
 
 module.exports = generators.Base.extend({
 
-    prompting : function() {
+    prompting: function() {
         /* Prompt for module name and basic structuring */
         return this.prompt([{
-            type : 'input',
-            name : 'ctrlNamespace',
-            message : 'Type module.controller name '
+            type: 'input',
+            name: 'ctrlNamespace',
+            message: 'Type module.controller name '
         }]).then(function(answers) {
             options = answers;
-            var ctrlNames = options.ctrlNamespace.split('.');
+            let ctrlNames = options.ctrlNamespace.split('.');
             options.moduleName = ctrlNames[0];
-            options.ctrlName = ctrlNames[1];
+            options.ctrlName = _.camelCase(ctrlNames[1]);
+            options.ctrlFileName = `${_.kebabCase(ctrlNames[1])}-controller.js`;
         }.bind(this));
     },
-    generateController : function(){
-        var modulePath = options.moduleName + '-module';
+    generateController: function() {
+        let modulePath = options.moduleName + '-module';
 
+        // Copy ctrl template.
         this.fs.copyTpl(
             this.templatePath('controller.js'),
-            this.destinationPath(modulePath + '/controller/' + options.ctrlName + '-controller.js'),
+            this.destinationPath(modulePath + '/controller/' + options.ctrlFileName),
             options
         );
 
+        // Initialize the ctrl in the module's controller index file.
+        let ctrlIndexFilePath = `${modulePath}/controller/index.js`;
+        let ctrlIndexFile = this.fs.read(ctrlIndexFilePath);
+        let lastLinePos = ctrlIndexFile.lastIndexOf('}');
+
+        // Read the last '}' character and
+        // insert the ctrl init code on the line before it.
+        let indexOutput = ctrlIndexFile.substr(0, lastLinePos) + `    app.controller('${options.moduleName}.${_.camelCase(options.ctrlName)}Controller', require('./${_.kebabCase(options.ctrlName)}-controller'));\n` + ctrlIndexFile.substr(lastLinePos);
+
+        this.fs.write(ctrlIndexFilePath, indexOutput);
     }
 });
